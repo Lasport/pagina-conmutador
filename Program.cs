7a67dcba6893b43879a4c5dc1ac162ac290cc296
Lasport <66816239+Lasport@users.noreply.github.com>
2021-07-04 18:27:54 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pagina_conmutador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
